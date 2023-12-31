cb8e8126df3d2256a20c1dcfcc7e171f9f16eecb
ceeza001 <mesalokubor@gmail.com>
2023-12-31 16:07:58 +0000

    Updated files

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
